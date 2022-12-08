fafa323460ac7e9291ad300175621558dfa77dfe
Eduard <eduardvagin14@gmail.com>
2022-12-08 20:08:16 +0200

    using modules

---
+++ js/modules/modal.js
@@ -0,0 +1,56 @@
+function closeModal(modalSelector) {
+    const modal = document.querySelector(modalSelector);
+
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+}
+
+function openModal(modalSelector, modalTimerID) {
+    const modal = document.querySelector(modalSelector);
+
+    modal.classList.add('show');
+    modal.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+
+    console.log(modalTimerID);
+    if (modalTimerID) {
+        clearInterval(modalTimerID);
+    }
+}
+
+function modal(triggerSelector, modalSelector, modalTimerID) {
+    //Modal
+
+    const modal = document.querySelector(modalSelector),
+    modalTrigger = document.querySelectorAll(triggerSelector);
+
+    modalTrigger.forEach(item => {
+        item.addEventListener('click', () => openModal(modalSelector, modalTimerID));
+    });
+
+    modal.addEventListener('click', (e) => {
+        if (e.target === modal || e.target.getAttribute('data-close') == '') {
+            closeModal(modalSelector);
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if (e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModal(modalSelector);
+        }
+    });
+
+    function showModalByScroll() {
+        if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
+            openModal(modalSelector, modalTimerID);
+            window.removeEventListener('scroll', showModalByScroll);
+        }
+    }
+
+    window.addEventListener('scroll', showModalByScroll);
+}
+
+export default modal;
+export {closeModal};
+export {openModal};
